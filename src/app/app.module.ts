d8687a1b9eebaa7a70a427954665d86c5c44e849
Jupiter <jupiter.fierce.31@gmail.com>
2018-06-13 11:28:59 +0300

    angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/homeModule/homeComponent/home.component';
 
+const appRoutes = [
+  {
+    path: 'home',
+    loadChildren: './modules/homeModule/home.module#HomeModule'
+  },
+  {
+    path: '',
+    component: HomeComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
