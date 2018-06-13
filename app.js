cd0487f1942f1a6f9c45bffb89225e0e1e58f963
Jupiter <jupiter.fierce.31@gmail.com>
2018-06-13 08:44:12 +0300

    app setting

--- app.js
+++ app.js
@@ -35,6 +35,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
+// app.use('/', express.static(path.join(__dirname, 'dist')));
 
 // setup the logger
 // app.use(morgan('combined', {stream: accessLogStream}))
